859c59f1dce96613adc093e7ea56b10582476c3f
Ednildo Cunha <45716508+EdnildoCunha@users.noreply.github.com>
2020-07-16 12:30:57 -0300

    Add files via upload

--- client.js
+++ client.js
@@ -1,17 +1,102 @@
 //https://edrestaurante.herokuapp.com/consulta/
+//Consulta menu
 const axios = require('axios');
-axios.get('https://edrestaurante.herokuapp.com/consulta/')
+/*
+axios.get('https://edrestaurante.herokuapp.com/menu/')
     .then(function (response){
-        const cardapio = response.data;
-        console.log("Primeiro lanche"+ JSON.stringify(cardapio[0]));
+        const menu = response.data;
+        console.log(JSON.stringify(menu));
     })
-
-
     .catch(function (error){
         console.log(error);
     })
-
-
     .then(function () {
         console.log("Execução concluída!");
     });
+*/
+//Cadastrar lanche
+/*
+axios.post("https://edrestaurante.herokuapp.com/menu/cadastrar", {
+   'lanche': 'hotdog',
+   'ingredientes': 'pão e salsicha',
+   'preco' : '10.00'
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+    .then(function (response) {
+        console.log(response.data);
+    });
+
+*/
+
+//Excluir lanche
+
+/*
+axios.post("https://edrestaurante.herokuapp.com/menu/excluir", {
+    'indice': '0'
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+    .then(function (response) {
+    console.log(response.data);
+});
+*/
+
+//Consultar carrinho
+/*
+axios.get("https://edrestaurante.herokuapp.com/carrinho")
+    .then(function (response) {
+    const carrinho = response.data;
+    console.log("Itens no carrinho")
+    console.log(JSON.stringify(carrinho));
+});
+*/
+
+//Adicionar item ao carrinho
+/*
+axios.post("https://edrestaurante.herokuapp.com/carrinho/cadastrar", {
+    'indice': '0'
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+    .then(function (response) {
+        console.log(response.data);
+    });
+*/
+//Excluir item do carrinho
+/*
+axios.post("https://edrestaurante.herokuapp.com/carrinho/excluir", {
+    'indice': '0'
+    })
+    .catch(function (error){
+        console.log(error);
+    })
+    .then(function (response) {
+        console.log(response.data);
+    });
+*/
+//Cadastro de endereço
+/*
+axios.post("https://edrestaurante.herokuapp.com/compra/endereco", {
+    'endereco': 'Rua Leoncio Castro, III conj da Cohab'
+    });
+   .catch(function (error){
+        console.log(error);
+    })
+    .then(function (response) {
+        console.log(response.data);
+    });
+*/
+
+//Compra
+/*
+axios.get("https://edrestaurante.herokuapp.com/compra")
+.then(function (response) {
+    const carrinho = response.data;
+    console.log("Confirmada compra! \n" + JSON.stringify(carrinho))
+
+});
+*/
